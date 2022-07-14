3636ab485e225f832d8319f82e3b51edf331056c
Benedict Osei Sefa <benedictoseisefa@Benedicts-MacBook-Pro.local>
2022-07-14 09:41:55 +0000

    Added style to the application

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: (Center(
-          child: (Column(
+        body: Center(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("Why do men sleep after Sex"),
-              Text("Ans: No time for bullshit"),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 25),
+                child: Text(
+                  "Which church will lead mankind to heaven?",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.brown,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+              ),
+              ),
+
+
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 10, 0, 40),
+                child: Text("Ans: Church Of Christ",
+                style: TextStyle(
+                  color: Colors.orange,
+                ),
+                ),
+              ),
+
+              
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  FloatingActionButton(onPressed: null,
-                  child: Icon(Icons.arrow_circle_left_rounded),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 30, 0),
+                    child: FloatingActionButton(onPressed: null,
+                    child: Icon(
+                      Icons.arrow_circle_left_rounded,
+                      size: 30,
+                      ),
+                    backgroundColor: Colors.orange,
+                  ),
                   ),
                   FloatingActionButton(onPressed: null,
-                  child: Icon(Icons.arrow_circle_right_rounded),
+                    child: Icon(
+                      Icons.arrow_circle_right_rounded,
+                      size: 30,
+                      ),
                   ),
-              ],)
+                ],
+              )
             ],
-          )),
-        )),
+          ),
+        ),
       ),
     );
   }
